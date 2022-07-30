3060d1c8160e57dc7d642acc9187ab751827395d
Sriram <sriram135175@gmail.com>
2022-07-30 20:09:53 +0530

    hope

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,10 @@
-const server = require('./server');
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const PORT = process.env.PORT || 80
+const path = require('path')
 
-const express = require('express');
-const cors = require('cors');
 
-const app = express();
-app.use(cors())
-console.log('Server Runnin');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server listening to -> ${PORT}`);
-});
-
-server();
+// In dev mode just hide hide app.uss(... ) below
+app.use( express.static(path.join(__dirname, '../build')))
+server.listen( PORT, () => console.log('Server runnin @ ' + PORT ))
