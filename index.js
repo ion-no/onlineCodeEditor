a692f979fe3fb507ca63af17155c4f4dd1a26cc3
Sriram <sriram135175@gmail.com>
2022-07-31 00:04:58 +0530

    .

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const path = require('path')
 
 
 // In dev mode just hide hide app.uss(... ) below
-app.use( express.static(path.join(__dirname, '../build')))
+    if (process.env.NODE_ENV === "production") {
+      app.use(express.static("build"));
+      app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+      });
+    }
 server.listen( PORT, () => console.log('Server runnin @ ' + PORT ))
